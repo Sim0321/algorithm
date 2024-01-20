3be47e9ffc7359aec4dc3e5c087241828b858a0d
Sim <0doogadooga@gmail.com>
2024-01-20 10:50:11 +0900

    소문자로 바꾸기 완료 및 다른 풀이 방법

---
+++ 소문자.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+// 제한 사항
+// 1 ≤ myString의 길이 ≤ 100,000
+// myString은 알파벳으로 이루어진 문자열입니다.
+
+function solution(myString) {
+  return myString.toLowerCase();
+}
+
+// 해설
+// 다른 사람 풀이보니 아스키코드 이용해서 푸는 방법이 있었다.
