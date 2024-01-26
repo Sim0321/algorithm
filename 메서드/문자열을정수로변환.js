05569175ab803e2656e86076ff6b9cdc798517c5
Sim <0doogadooga@gmail.com>
2024-01-26 14:56:20 +0900

    문자열을 정수로 변환 완료

---
+++ 메서드/문자열을정수로변환.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+// 제한 사항
+// 1 ≤ n_str ≤ 5
+// n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
+
+const solution = (n_str) => {
+  return Number(n_str);
+};
+
+// 해설
