48de0230a09b69f82f6eea047fc39f2822682fc0
Sim <0doogadooga@gmail.com>
2024-02-10 23:26:46 +0900

    특정 문자열로 끝나는 가장 긴부분문자열 찾기 수정 완료

--- 문자열/특정문자열로끝나는가장긴부분문자열찾기.js
+++ 문자열/특정문자열로끝나는가장긴부분문자열찾기.js
@@ -8,3 +8,5 @@ console.log(solution("AAAAaaaa", "a"));
 
 // 해설 및 다른 사람 풀이
 // pat이 여러번 등장한다면 마지막 인덱스를 알아야하는 문제
+// const solution = (str, pat) => str.substring(0, str.lastIndexOf(pat)) + pat;
+// substring은 잘 사용하지 않아서 모르는데 한번 공부가 필요하다.
