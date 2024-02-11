9a527ea2113751b6ffbbd611d8a672b637b0472a
Sim <0doogadooga@gmail.com>
2024-02-11 16:48:40 +0900

    배열 만들기5 완료

--- 문자열/배열만들기5.js
+++ 문자열/배열만들기5.js
@@ -4,7 +4,7 @@
 // 배열 intStrs의 각 원소마다 s번 인덱스에서 시작하는 길이 l짜리 부분 문자열을 잘라내 정수로 변환합니다. 이때 변환한 정수값이 k보다 큰 값들을 담은 배열을 return 하는 solution 함수를 완성해 주세요.
 
 const solution = (strs, k, s, l) => {
-  return strs.map((el) => Number(el.slice(s, s + l))).filter((el) => el > k);
+  return strs.map((el) => Number(el.slice(s, s + l))).filter((v) => v > k);
 };
 
 console.log(
