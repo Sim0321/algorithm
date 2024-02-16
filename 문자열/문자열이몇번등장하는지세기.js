a1ba4bf22d3d483de325450046f056dde70df0e9
Sim <0doogadooga@gmail.com>
2024-02-17 02:45:02 +0900

    문자열이 몇 번 등장하는지 세기 완료

--- 문자열/문자열이몇번등장하는지세기.js
+++ 문자열/문자열이몇번등장하는지세기.js
@@ -1,10 +1,16 @@
 // 문제 설명
+// 문자열 myString과 pat이 주어집니다. myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
 
-const solution = () => {
-  console.log();
-  // return
+const solution = (myString, pat) => {
+  let answer = 0;
+  for (let i = 0; i <= myString.length - pat.length; i++) {
+    if (myString.slice(i, i + pat.length) === pat) {
+      answer++;
+    }
+  }
+  return answer;
 };
 
-console.log(solution());
+console.log(solution("banana", "ana"));
 
 // 해설 및 다른 사람 풀이
