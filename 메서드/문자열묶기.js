c272340f6b0444261f00536540c2bf3104529416
Sim <0doogadooga@gmail.com>
2024-02-26 22:23:30 +0900

    문자열 묶기 완료

---
+++ 메서드/문자열묶기.js
@@ -0,0 +1,19 @@
+// 문제 설명
+// 문자열 배열 strArr이 주어집니다. strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성해 주세요.
+
+const solution = (strArr) => {
+  const obj = {};
+  strArr.forEach((el) => {
+    if (obj[el.length] !== undefined) {
+      obj[el.length] += 1;
+    } else if (obj[el.length] === undefined) {
+      obj[el.length] = 1;
+    }
+  });
+
+  return Math.max(...Object.values(obj));
+};
+
+console.log(solution(["a", "bc", "d", "efg", "hi"]));
+
+// 해설 및 다른 사람 풀이
