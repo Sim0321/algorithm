2437cc154ae4012f44f0c60bda331b75303d436e
Sim <0doogadooga@gmail.com>
2024-03-12 09:59:34 +0900

    조건문자열 다른 사람 풀이 추가

--- 조건문/조건문자열.js
+++ 조건문/조건문자열.js
@@ -20,3 +20,14 @@ const solution = (ineq, eq, n, m) => {
 console.log(solution(">", "!", 20, 50));
 
 // 해설 및 다른 사람 풀이
+// const operations = {
+//   '>=': (n, m) => n >= m,
+//   '<=': (n, m) => n <= m,
+//   '>!': (n, m) => n > m,
+//   '<!': (n, m) => n < m,
+// };
+
+// function solution(ineq, eq, n, m) {
+//   const op = operations[ineq + eq];
+//   return Number(op(n, m));
+// }
